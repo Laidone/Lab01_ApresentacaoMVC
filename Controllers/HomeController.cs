6e312223cb98d358c765641ae819e0eafe7220f0
Laidone <laidonecarvalho@hotmail.com>
2021-11-24 10:22:08 -0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿/*
+ Nome: Laidone Mendes de Carvalho
+ Lab01_ApresentacaoMVC
+ */
+
+using Microsoft.AspNetCore.Mvc;
+using Lab01_ApresentacaoMVC.Models;
+
+namespace Lab01_ApresentacaoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        /*public string Index()
+        {
+            return "Hello MVC!";
+        }*/
+        public ActionResult Index()
+        {
+            @ViewBag.Agora = "Hello MVC! " + DateTime.Now;
+            return View();
+        }
+        public ActionResult Filme()
+        {
+            Filme filme = new Filme()
+            {
+                Titulo = "Inglourious Basterds",
+                Genero = "War",
+                Assistido = true
+            };
+            return View(filme);
+        }
+
+    }
+}
---
+++ Models/Filme.cs
@@ -0,0 +1,10 @@
+﻿namespace Lab01_ApresentacaoMVC.Models
+{
+    public class Filme
+    {
+        public String Titulo { get; set; }
+        public String Genero { get; set; }
+        public bool Assistido { get; set; }
+    }
+
+}
